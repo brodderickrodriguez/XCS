042a9eec7d0ab3580a9dc0daaa3b720c42d67c78
brodderickrodriguez <bcr@brodderick.com>
2019-06-28 17:58:58 -0500

    implemented xor scenario using new structure

--- xcs/example_scenarios/xor/xor_environment.py
+++ xcs/example_scenarios/xor/xor_environment.py
@@ -1,8 +1,28 @@
+# Brodderick Rodriguez
+# Auburn University - CSSE
+# June 28 2019
 
 from xcs.environment import Environment
+import numpy as np
 
 
 class XOREnvironment(Environment):
     def __init__(self):
         Environment.__init__(self)
         print('initialized XOR environment')
+
+        self.state = None
+        self.state_length = 0
+        self.step(None)
+
+    def get_state(self):
+        return self.state
+
+    def step(self, action):
+        self.state = [int(round(np.random.uniform())) for _ in range(2)]
+
+    def reset(self, action):
+        self.step(None)
+
+    def print_world(self):
+        print(self.state)
--- xcs/example_scenarios/xor/xor_reinforcement_program.py
+++ xcs/example_scenarios/xor/xor_reinforcement_program.py
@@ -1,3 +1,6 @@
+# Brodderick Rodriguez
+# Auburn University - CSSE
+# June 28 2019
 
 from xcs.reinforcement_program import ReinforcementProgram
 
@@ -6,3 +9,10 @@ class XORReinforcementProgram(ReinforcementProgram):
     def __init__(self):
         ReinforcementProgram.__init__(self)
         print('initialized XOR reinforcement program')
+
+    def determine_rho(self, sigma, action):
+        self.step()
+
+        rho = int(sigma[0] ^ sigma[1] == action)
+
+        return rho
