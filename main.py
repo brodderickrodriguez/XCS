bb7fa995a2eae3bb9ed969f733948d31d9648759
brodderickrodriguez <bcr@brodderick.com>
2019-07-10 17:32:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import logging
 import sys
+from xcs.xcs_driver import XCSDriver
+from xcs.xcs import XCS
 from xcs import util
 
 
@@ -26,15 +28,12 @@ def human_play_woods2():
 
 
 def xor():
-    from xcs.xcs_driver import XCSDriver
-    from xcs.xcs import XCS
-
     from xcs.example_scenarios.xor.xor_environment import XOREnvironment
     from xcs.example_scenarios.xor.xor_reinforcement_program import XORReinforcementProgram
     from xcs.example_scenarios.xor.xor_configuration import XORConfiguration
 
     driver = XCSDriver()
-    driver.repetitions = 30
+    driver.repetitions = 20
     driver.save_location = './xcs/example_scenarios/xor/data'
     driver.experiment_name = 'test1'
 
@@ -47,17 +46,14 @@ def xor():
 
 
 def multiplexer():
-    from xcs.xcs_driver import XCSDriver
-    from xcs.xcs import XCS
-
     from xcs.example_scenarios.multiplexer.multiplexer_environment import MultiplexerEnvironment
     from xcs.example_scenarios.multiplexer.multiplexer_reinforcement_program import MultiplexerReinforcementProgram
     from xcs.example_scenarios.multiplexer.multiplexer_configuration import MultiplexerConfiguration
 
     driver = XCSDriver()
-    driver.repetitions = 10
+    driver.repetitions = 30
     driver.save_location = './xcs/example_scenarios/multiplexer/data'
-    driver.experiment_name = 'test4'
+    driver.experiment_name = 'test1'
 
     driver.xcs_class = XCS
     driver.environment_class = MultiplexerEnvironment
@@ -68,17 +64,14 @@ def multiplexer():
 
 
 def woods2():
-    from xcs.xcs_driver import XCSDriver
-    from xcs.xcs import XCS
-
     from xcs.example_scenarios.woods2.woods2_environment import Woods2Environment
     from xcs.example_scenarios.woods2.woods2_reinforcement_program import Woods2ReinforcementProgram
     from xcs.example_scenarios.woods2.woods2_configuration import Woods2Configuration
 
     driver = XCSDriver()
-    driver.repetitions = 30
+    driver.repetitions = 10
     driver.save_location = './xcs/example_scenarios/woods2/data'
-    driver.experiment_name = 'test2'
+    driver.experiment_name = 'test1'
 
     driver.xcs_class = XCS
     driver.environment_class = Woods2Environment
@@ -100,4 +93,4 @@ if __name__ == '__main__':
     woods2()
 
     dir_name = './xcs/example_scenarios/woods2/data/test1'
-    util.plot_results2(dir_name, title='W2', interval=50)
+    util.plot_results(dir_name, title='W2', interval=50)
